f796c42639dbc47b98ec94b7918e794a44cbbc24
sadpandaemoji <sadpandaemoji@gmail.com>
2022-10-31 15:42:51 -0400

    add main functionality

--- backend/scraper.js
+++ backend/scraper.js
@@ -7,9 +7,15 @@ fetch('https://apnews.com/hub/world-news')
     .then(response=>response.text())
     .then(data=>{
         const domParsed = new JSDOM(data)
-        const domDocument = domParsed.window.document.querySelectorAll('div.FeedCard h2')
+        //im going to try to iterate through the parent node FeedCard. then get the associated h2 and a and p. of the node. grab the alt from the image if possible.
+        //see old puppeteer scraper for exact querying. however, the image seems to be under div.feedcard > a > div.media-placeholder > img --- i think there might be a method to grab the 'alt'
+        const domDocument = domParsed.window.document.querySelectorAll('div.FeedCard')
         for(let i=0;i<domDocument.length;i++){
-            console.log(domDocument[i].textContent)
+            console.log("Article " + (i + 1))
+            console.log(domDocument[i].querySelector('h2') ? domDocument[i].querySelector('h2').textContent : "empty")
+            console.log(domDocument[i].querySelector('a') ? "https://apnews.com" + domDocument[i].querySelector('a').href: "no provided hyperlink")
+            console.log(domDocument[i].querySelector('p') ? domDocument[i].querySelector('p').textContent : "no provided summary")
+            console.log(domDocument[i].querySelector('span.Timestamp') ? domDocument[i].querySelector('span.Timestamp').textContent : "no provided timestamp")
         }
     })
     .catch(error=>console.log(error))
