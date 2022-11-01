e6bf066f751389345c868556252a1ba40051b7b2
sadpandaemoji <sadpandaemoji@gmail.com>
2022-11-01 06:03:19 -0400

    finish api news route code

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+const scraper = require("./scraper")
+let dataContainer = []
 
+const placeData = async ()=>{
+    dataContainer = await scraper()
+}
+
+placeData()
 
 app.get('/', (req,res)=>{
-    res.send("hello, basic node and express server running.")
+    res.send("scraper called, check /api route")
+})
+
+//i have a feeling this might not be the most efficient way to do this.
+//i want to have it already called? and then respond with the data. 
+//i might have to save it to a variable outside of this get request. 
+//i think the way that i have it now, its calling the scraper() only when a get request is sent.
+//i want it already to have it by the time the get request is sent. 
+
+//fixed it, put it in a callback function that saves it in here. i forget the terminology for like global vs local variables? 
+//issues came up, the log said i cant assign the data to const array variable, so i changed to let. 
+//its working now, and i filtered out the empty ones within the scraper, at the bottom. same deal with global/local variable stuff.
+app.get('/api/news', (req,res)=>{
+    res.send(dataContainer)
 })
 
 app.listen(port, ()=>{
--- backend/scraper.js
+++ backend/scraper.js
@@ -1,24 +1,28 @@
 const fetch = require("node-fetch")
 const jsdom = require("jsdom")
 const {JSDOM} = jsdom
-const dataArray = []
+let dataArray = []
 
-fetch('https://apnews.com/hub/world-news')
-    .then(response=>response.text())
-    .then(data=>{
-        const domParsed = new JSDOM(data)
-        const domDocument = domParsed.window.document.querySelectorAll('div.FeedCard')
-        for(let i=0;i<domDocument.length;i++){
-            const dataObject = {
-                headline:domDocument[i].querySelector('h2') ? domDocument[i].querySelector('h2').textContent : "empty",
-                hyperlink:domDocument[i].querySelector('a') ? "https://apnews.com" + domDocument[i].querySelector('a').href: "no provided hyperlink",
-                summary:domDocument[i].querySelector('p') ? domDocument[i].querySelector('p').textContent : "no provided summary",
-                timestamp:domDocument[i].querySelector('span.Timestamp') ? domDocument[i].querySelector('span.Timestamp').textContent : "no provided timestamp"
-            }
-            dataArray[i] = dataObject
+const scraper = async () => {
+    const response = await fetch('https://apnews.com/hub/world-news')
+    const data = await response.text()
+    const domParsed = new JSDOM(data)
+    const domDocument = domParsed.window.document.querySelectorAll('div.FeedCard')
+    for(let i=0;i<domDocument.length;i++){
+        const dataObject = {
+            headline:domDocument[i].querySelector('h2') ? domDocument[i].querySelector('h2').textContent : "empty",
+            hyperlink:domDocument[i].querySelector('a') ? "https://apnews.com" + domDocument[i].querySelector('a').href: "no provided hyperlink",
+            summary:domDocument[i].querySelector('p') ? domDocument[i].querySelector('p').textContent : "no provided summary",
+            timestamp:domDocument[i].querySelector('span.Timestamp') ? domDocument[i].querySelector('span.Timestamp').textContent : "no provided timestamp"
         }
-        console.log(dataArray)
-    })
-    .catch(error=>console.log(error))
+        dataArray[i] = dataObject
+    }
+    dataArray = dataArray.filter(item=>item.headline!="empty")
+    console.log("scraper called, testing to see if its called when a get request is sent")
+    return dataArray
+}
 
+
+
+module.exports = scraper
 
