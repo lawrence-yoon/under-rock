1f84ffa0280791d63cfb04ff1f09c7938ab5ded6
sadpandaemoji <sadpandaemoji@gmail.com>
2022-11-06 12:57:00 -0500

    clean up comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,6 @@ function App() {
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
-  //i want to insert a prompt, prompting whoever to input name
-
-  //maybe make a "timeNow" state, stick it in useEffect, live updating.
-  //i tried taking out the brackets at the end of useEffect, but then it just kept calling.
-  //need to look further into useEffect
   
   useEffect(()=>{
     //this fetch is to get the data from the backend, proxy set to 
@@ -21,7 +16,6 @@ function App() {
     //get request sent, data received should be json object with three 
     //key value pairs, the keys with array in the name are matched with arrays
     //these arrays should be used to dynamically render components
-    //weather should be able to be hardcoded
 
     fetch('/api/news')
     .then(response=>{
@@ -43,10 +37,6 @@ function App() {
     })
 
 
-  //the square brackets missing on bottom, so that it can update the time.
-  //might consider a different approach, while this renders every second, 
-  //it is also sending the get request constantly, as shown in backend terminal
-
   },[])
   
   if(isLoading) return "Loading..."
@@ -59,13 +49,8 @@ function App() {
         <Weather {...data.weatherAPI}/>
       </div>
       <div className="news-container">
-        {/* <div className='currencies-container'>
-            <p>test</p>
-            <p>test2</p>
-            <p>test3</p>
-        </div> */}
         <div className="articles-container">
-        <h2 style={{alignSelf: "flex-start", margin: "8px", padding: "8px", flexBasis:"100%"}}>AP News</h2>
+        <h2 style={{alignSelf: "flex-start", margin: "8px", padding: "8px", flexBasis:"100%"}}>AP News - International</h2>
         <br />
           {data.newsAPArray.map((article)=>
               <NewsAP {...article}/>
--- frontend/src/components/Greeting.jsx
+++ frontend/src/components/Greeting.jsx
@@ -7,11 +7,9 @@ function Greeting() {
     setInterval(()=> setCurrentTime(new Date().toLocaleTimeString("en-US")), 1000)
   },[])
   return (
-    //i want to include some logic to display Good Morning or something...
-    //Also, when I have the barebones app done, i want to make it so i pass through location as well
-    <div className='greeting'>
-        <h2>Hello Larry.</h2>
-        <h3 className='time'>{currentTime}</h3>
+    <div className='clock'>
+        <h1 className='time'>{currentTime}</h1>
+        <h2>Queens, NY</h2>
     </div>
   )
 }
