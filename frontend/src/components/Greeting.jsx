8e409eeedd899ac8f3d6d5f265883563e504ae5f
sadpandaemoji <sadpandaemoji@gmail.com>
2022-11-11 22:03:45 -0500

    add greeting functionality and increase font size

--- frontend/src/components/Greeting.jsx
+++ frontend/src/components/Greeting.jsx
@@ -1,14 +1,31 @@
 import React, {useState, useEffect} from 'react'
 
 function Greeting() {
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString("en-US"))
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const goodGreeting = function(){
+    let milTimeHours = currentTime.toLocaleTimeString("en-US", {hour12:false}).slice(0,2)
+    if(0<=milTimeHours && milTimeHours<=5){
+      return "Burning the Midnight Oil, Larry?"
+    }
+    if(5<milTimeHours && milTimeHours<12){
+      return "Good Morning, Larry."
+    }
+    if(12<milTimeHours && milTimeHours<17){
+      return "Good Afternoon, Larry."
+    }
+    if(17<milTimeHours && milTimeHours<24){
+      return "Good Evening, Larry."
+    }
+    return "error"
+  }
 
   useEffect(()=>{
-    setInterval(()=> setCurrentTime(new Date().toLocaleTimeString("en-US")), 1000)
+    setInterval(()=> setCurrentTime(new Date()), 1000)
   },[])
   return (
     <div className='clock'>
-        <h1 className='time'>{currentTime}</h1>
+        <h1 className='time'>{currentTime.toLocaleTimeString("en-US").slice(0,8)}</h1>
+        <h2 className='good-greeting'>{goodGreeting()}</h2>
         <h2>Queens, NY</h2>
     </div>
   )
